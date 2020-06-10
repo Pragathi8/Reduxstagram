7694778b76a5be4cd50d34c88ae0b232f2781750
Pragathi Nataraj <pragathi.nataraj@publicissapient.com>
2020-06-10 13:47:42 +0530

    complete redux setup

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,29 @@
+function PostComments(state = [], action) {
+  switch (action.type) {
+    case "ADD_COMMENT":
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment
+        }
+      ];
+    case "REMOVE_COMMENT":
+      console.log("removing comment");
+      return [...state.slice(0, action.i), ...state.slice(action.i + 1)];
+    default:
+      return state;
+  }
+}
+
+function comments(state = [], action) {
+  if (typeof action.postId !== "undefined") {
+    return {
+      ...state,
+      [action.postId]: PostComments(state[action.postId], action)
+    };
+  }
+  return state;
+}
+
+export default comments;
